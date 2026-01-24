c4ee0f01c9274928a25d23863111f2b90eb779c8
Aman Kushwah <executive5kloudon@gmail.com>
2026-01-24 15:16:57 +0530

    First commit

---
+++ react-basic/src/App.jsx
@@ -0,0 +1,14 @@
+
+function Card(){
+  return <div className="parent">
+    <div className="center">
+      <h1>Aman Kushwah</h1>
+      <p></p>
+      <img src="profile.png" alt="profile pic"></img>
+    </div>
+    <div className="bottom">
+      <h1>Read More</h1>
+    </div>
+  </div>
+}
+export default Card;
---
+++ ui-project/src/assets/components/section1/Section1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-gray-700'>
+        <div className='bg-green-400'>
+      <nav className='nav'>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Contact</a></li>
+            <li><a href='#'>More</a></li>
+        </ul>
+      </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Section1
