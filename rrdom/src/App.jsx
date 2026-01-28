081f7d56bc93805d0a21332a948de9497466e436
Aman Kushwah <executive5kloudon@gmail.com>
2026-01-28 17:06:23 +0530

    feat: add navbar component and update routing in App.jsx

--- rrdom/src/App.jsx
+++ rrdom/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './conponents/home'
 import About from './conponents/about'
+import Contact from './conponents/contact'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
       </Routes>
     </div>
   )
---
+++ rrdom/src/conponents/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href='./home.jsx'>Home</a>
+        <a href='./contact.jsx'>Contact</a>
+        <a href='./about.jsx'>About</a>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
